f2e1de53235e5d0a69d42f57cde4bd3f3d7a4ef5
btactic-translate-2 <btactic-translate-2@btactic.com>
2016-02-08 09:23:15 +0100

    Post install script fix

--- scripts/post_install.php
+++ scripts/post_install.php
@@ -1,6 +1,9 @@
 <?php
 if (! defined('sugarEntry') || ! sugarEntry) die('Not A Valid Entry Point');
 function post_install() {
-  $_REQUEST['js_admin_repair']='mini';
+  global $current_user;
+  $_REQUEST['type'] = "mini";
+  require_once("include/utils.php");
   require_once("modules/Administration/callJSRepair.php");
+  require_once("modules/Administration/RepairJSFile.php");
 }
